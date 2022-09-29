67a7e312f7aba40d62d3d9d391dff4a9dd2c8cc1
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-09-29 11:07:14 -0300

    Lista 3 - até exercício 6

--- ListaRepeticao/Program.cs
+++ ListaRepeticao/Program.cs
@@ -5,6 +5,7 @@ using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ListaRepeticao
 {
@@ -238,6 +239,51 @@ namespace ListaRepeticao
 
             //Console.WriteLine("Quantidade de eleitores: " + total);
             //Console.WriteLine("Votos nulos: " + nulo);
+
+            //       6.Faça um programa em VS que solicite ao o usuário que informe a idade e valide a entrada de dados, 
+            //       ou seja, repita a leitura até que ela esteja correta(maior do que zero). 
+            //       Ao final, mostre a idade digitada.
+
+            //int idade = 0;
+
+            //while (idade <= 0)
+            //{
+
+            //    Console.WriteLine("Informe a idade - maior que zero: ");
+            //    idade = int.Parse(Console.ReadLine());
+
+            //    if (idade > 0)
+            //    {
+            //        Console.WriteLine("A idade digitada é: " + idade + " anos!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Valor incorreto. Favor digitar uma idade maior que zero.\n" +
+            //            "");
+            //    }
+
+            //}
+
+            //            7.Faça um programa em VS que solicite o nome, a idade e o salário de uma pessoa.
+            //A cada solicitação, teste se o usuário informou um valor válido. 
+            //Por exemplo: se o nome for não for lido corretamente (ou seja, se estiver em branco ou se for um número), 
+            //informe que ele está incorreto e saia do programa em VS.
+            //Se o nome for lido corretamente, solicite a idade.Se ela for incorreta(menor ou igual a zero),
+            //informe que está errada e saia.Se estiver correta, solicite o salário.
+            //Se ele estiver incorreto(menor ou igual a zero), informe que está incorreto e saia.
+            //Se estiver correto, mostre todos os valores lidos.
+
+            String nome;
+            int idade = 0;
+            double salario = 0;
+
+            nome = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(nome) || nome.Any(char.IsDigit))
+            {
+
+            }
+
 
         }
     }
