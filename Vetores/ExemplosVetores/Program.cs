03a98c3e53bce044fe59de77c7da74ea33656ecd
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-09-29 14:34:45 -0300

    Iniciando vetores

---
+++ Vetores/ExemplosVetores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemplosVetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+            int[] b = new int[5];
+            Console.WriteLine("------ Leitura dos valores: ------");
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite o valor " + i + ":");
+                b[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Valore índice " + i + " é igual a " + b[i]);
+            }
+
+        }
+    }
+}
