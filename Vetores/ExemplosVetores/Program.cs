d4be1f41f177f5fd495acd417a4d865f32670bbc
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-09-29 15:30:01 -0300

    Exemplos de vetores

--- Vetores/ExemplosVetores/Program.cs
+++ Vetores/ExemplosVetores/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,21 +12,95 @@ namespace ExemplosVetores
         static void Main(string[] args)
         {
 
-            int i;
-            int[] b = new int[5];
-            Console.WriteLine("------ Leitura dos valores: ------");
+            //int i;
+            //int[] b = new int[5];
+            //Console.WriteLine("------ Leitura dos valores: ------");
 
-            for (i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Digite o valor " + i + ":");
-                b[i] = int.Parse(Console.ReadLine());
-            }
+            //for (i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Digite o valor " + i + ":");
+            //    b[i] = int.Parse(Console.ReadLine());
+            //}
 
-            for (i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Valore índice " + i + " é igual a " + b[i]);
-            }
+            //for (i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Valore índice " + i + " é igual a " + b[i]);
+            //}
 
+            //double[] notas = new double[50];
+            //double media;
+            //int i;
+
+            //Console.WriteLine("------ Leitura dos valores ------");
+            //for (i = 0; i < 50; i++)
+            //{
+            //    Console.WriteLine("Informe a nota do aluno " + i + " :");
+            //    notas[i] = double.Parse(Console.ReadLine());
+            //}
+
+            //media = 0;
+
+            //Console.WriteLine("------ Cálculo da média sendo realizado ------");
+            //for (i = 0; i < 50; i++)
+            //{
+            //    media += notas[i];
+            //}
+
+            //media = media / 50;
+
+            //Console.WriteLine("------ Mostra a média ------");
+            //Console.WriteLine("A média das notas é igual a " + media);
+
+            //int[] idade = new int[40];
+            //int i, conta = 0;
+            //Console.WriteLine("Informe as idades dos alunos da turma: ");
+            //for (i = 0; i < 40; i++)
+            //{
+            //    idade[i] = int.Parse(Console.ReadLine());
+            //    if (idade[i] >= 18)
+            //    {
+            //        conta++;
+            //    }
+            //}
+            //Console.WriteLine("Existem " + conta + " alunos com idade igual ou maior que 18 anos!");
+            //for (i = 0; i < 40; i++)
+            //{
+            //    if (idade[i] >= 18)
+            //    {
+            //        Console.WriteLine("Aluno " + i + " tem " + idade[i] + " anos.");
+            //    }
+            //}
+
+            //int[] vetor = new int[10];
+            //int i, maior, menor, posicaoMaior = 0, posicaoMenor = 0;
+
+            //for (i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Digite o valor do elemento: "+i);
+            //    vetor[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //maior = vetor[0]; //defino que o primeiro é o maior
+            //menor = vetor[0]; //defino que o primeiro é menor
+            //for (i = 1; i < 10; i++) //pode começar no 1 porque já defini o primeiro como maior até então
+            //{
+            //    if (vetor[i] > maior)
+            //    {
+            //        maior = vetor[i];
+            //        posicaoMaior = i;
+            //    }
+
+            //    if (vetor[i] < menor)
+            //    {
+            //        menor = vetor[i];
+            //        posicaoMenor = i;
+            //    }
+            //}
+
+            //Console.WriteLine("O maior elemento é " + maior + " e está na posição " + posicaoMaior);
+            //Console.WriteLine("O menor elemento é " + menor + " e está na posição " + posicaoMenor);
+
+            
         }
     }
 }
