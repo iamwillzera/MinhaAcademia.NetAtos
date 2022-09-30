de460d764bca94cd6869464f2575c81427f5fdba
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-09-30 15:29:09 -0300

    Lista 4 até exercício 10

--- Vetores/ListaVetores/Program.cs
+++ Vetores/ListaVetores/Program.cs
@@ -69,6 +69,32 @@ namespace ListaVetores
             //Escreva um algoritmo que leia os valores para um vetor de 20 elementos e então
             //mostre na tela o índice da posição dos valores correspondentes a números primos.
 
+            //int[] vetor = new int[20];
+            //int i;
+
+            //for (i = 0; i < 20; i++)
+            //{
+            //    Console.WriteLine("Informe o valor da posição " + (i + 1) + ": ");
+            //    vetor[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (i = 0; i < 20; i++)
+            //{
+            //    int divisores = 0; //declarando dentro do for para que, a cada laço, o valor volte a zero
+
+            //    for (int x = 1; x <= vetor[i]; x++)
+            //    {
+            //        if (vetor[i] % x == 0)
+            //        {
+            //            divisores++;
+            //        }
+            //    }
+            //    if (divisores == 2)
+            //    {
+            //        Console.WriteLine("A posição "+(i+1)+" tem o valor " + vetor[i]+" que é um número primo!");
+            //    }
+            //}
+
             //----------------------------------------------------------------------------------------//
 
             //4. Escreva um algoritmo que leia dois vetores de 10 posições e faça a multiplicação dos
@@ -191,9 +217,61 @@ namespace ListaVetores
 
             //----------------------------------------------------------------------------------//
 
-            //7.Fazer um algoritmo que leia dez números inteiros armazenando-os em um vetor e escreva primeiramente todos os números pares lidos e após todos os ímpares. Exemplo:
-            //a.lê: 		| 7 | 40 | 3 | 9 | 21 | 0 | 63 | 31 | 7 | 22 |
-            //b.escreve: 	| 40 | 0 | 22 | 7 | 3 | 9 | 21 | 63 | 31 | 7 |
+            ////8.	Fazer um algoritmo que leia trinta números reais armazenando-os em um vetor e após escreva 
+            ///a posição de cada número menor que zero desse vetor. Exemplo:
+            //a.lê:          	| 5.1 | 0 | -3.6 | 4.1 | -2.5 | -1.3 | -4 | 1.39 | -128 | -6.9 | 8.2 | 9 | 154 | -88 | 6.4 | 7.1 | ...|
+            //b.escreve: 	| 3 | 5 | 6 | 7 | 9 | 10 | 14 |. . .
+
+            //double[] vetor = new double[30];
+            //int i, posicaoNegativo;
+
+            //for (i = 0; i < 30; i++)
+            //{
+            //    Console.WriteLine("Informe o valor: ");
+            //    vetor[i] = double.Parse(Console.ReadLine());
+            //}
+
+            //Console.Write("| ");
+            //for (i = 0; i < 30; i++)
+            //{
+            //    if (vetor[i] < 0)
+            //    {
+            //        posicaoNegativo = i;
+            //        Console.Write((posicaoNegativo + 1) + " | ");
+            //    }
+            //}
+
+            //-----------------------------------------------------------------------------------//
+
+            //9.Escreva um algoritmo que leia os valores para um vetor de 10 elementos, 
+            //e em seguida ordene em ordem crescente os valores desse vetor, utilizando um vetor auxiliar.
+
+            //int i;
+            //int[] vetor = new int[10], resultado = new int[10];
+
+            //for (i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Informe o valor: ");
+            //    vetor[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Array.Sort(vetor);
+
+            //foreach (int crescente in vetor)
+            //{
+            //    Console.WriteLine(crescente);
+
+            //}
+
+            //-----------------------------------------------------------------------------------------//
+            //10.Escreva um algoritmo que leia um vetor inteiro de 20 posições.Crie um segundo vetor, substituindo os valores nulos por 2.Mostre os vetores lidos e o vetor resultado.
+
+            int[] vetor = new int[20];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("Informe o valor da posição " + (i + 1) + ": ");
+            }
 
 
         }
