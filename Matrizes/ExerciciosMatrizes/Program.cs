ae0b5fe9493a072171b31af45dc4263a3f3f1be7
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-10-04 14:47:05 -0300

    Matrizes

---
+++ Matrizes/ExerciciosMatrizes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosMatrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1) Crie uma Matriz 5x3. Na primeira coluna, solicite que o usuário preencha.
+            //NA 2ª coluna, some 10 aos elementos da 1ª coluna.
+            //Na 3º coluna, armazene o dobro dos elementos da 1ª coluna.
+
+            //int[,] matriz = new int[5, 3];
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Informe o valor  para a coluna 0: ");
+            //    matriz[i, 0] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    matriz[i, 1] = matriz[i, 0] + 10;
+            //    matriz[i, 2] = matriz[i, 0] * 2;
+
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write("[" + i + "," + j + "] = " + matriz[i, j] + " ");
+            //}
+            //    Console.WriteLine();
+            //}
+            //--------------------------------------------------------------------------------------------------//
+
+            //2) Solicite ao usuário, preencher uma Matriz 3x3
+
+            //            Informe ao usuário:
+            //            *A soma dos elementos de cada linha
+            // -Ex: Linha 1: 30
+            //Linha 2: 17
+
+            //* A soma dos elementos de cada coluna
+
+            //-Ex: Coluna 1: 23
+            //Coluna 2: 36
+
+            //----------------------------------------------------------------------------------------//
+
+
+
+
+        }
+    }
+}
