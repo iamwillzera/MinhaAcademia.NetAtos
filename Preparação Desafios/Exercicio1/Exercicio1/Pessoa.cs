2977a37bce550674afad67c94349b1685d2654d4
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-10-24 11:22:45 -0300

    Reorganizando estrutura do repositório

---
+++ Preparação Desafios/Exercicio1/Exercicio1/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public class Pessoa
+    {
+
+        public string nome;
+        public Pessoa(string nomePessoa)
+        {
+            this.nome = nomePessoa;
+        }
+
+        public static void cadastrarNome()
+        {
+            string nome;
+            Console.WriteLine("Informe o nome: ");
+            nome = Console.ReadLine();
+
+        }
+    }
+}
---
+++ Preparação Desafios/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa.cadastrarNome();
+
+        }
+    }
+}
