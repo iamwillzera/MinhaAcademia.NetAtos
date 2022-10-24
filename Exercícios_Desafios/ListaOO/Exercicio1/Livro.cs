66c8359dd846a80c3dd38856d79d9f8c4c50f043
William Oliveira <101009161+iamwillzera@users.noreply.github.com>
2022-10-24 12:06:01 -0300

    Reorganizando estrutura do repositório

---
+++ Exercícios_Desafios/ListaOO/Exercicio1/Livro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Livro
+    {
+        public string titulo;
+        public int qtdePaginas;
+        public string autor;
+        public string isbn;
+
+        public Livro(string titulo, int qtdePaginas, string autor, string isbn)
+        {
+            this.titulo = titulo;
+            this.qtdePaginas = qtdePaginas;
+            this.autor = autor;
+            this.isbn = isbn;
+        }
+
+        public void dadosLivro(/*string titulo,string isbn,string autor,int qtdePaginas*/)
+        {
+            Console.WriteLine("Informe o título do livro: ");
+            titulo = Console.ReadLine();
+
+            Console.WriteLine("ISBN: ");
+            isbn = Console.ReadLine();
+
+            Console.WriteLine("Autor: ");
+            autor = Console.ReadLine();
+
+            Console.WriteLine("Quantidade de páginas: ");
+            qtdePaginas = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Exercícios_Desafios/ListaOO/Exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tituloLivro = "";
+            string codIsbn = "";
+            string nomeAutor = "";
+            int qtPaginas = 0;
+
+            Livro livro = new Livro(tituloLivro, qtPaginas, nomeAutor, codIsbn);
+
+            livro.dadosLivro(/*tituloLivro, codIsbn, nomeAutor, qtPaginas*/);
+
+            Console.WriteLine();
+            Console.WriteLine("--- Dados do livro ---");
+            Console.WriteLine();
+
+            Console.WriteLine($"Título: {livro.titulo}" +
+                $"\nCódigo ISBN: {livro.isbn}" +
+                $"\nAutor: {livro.autor}" +
+                $"\nQuantidade de páginas: {livro.qtdePaginas}");
+
+        }
+    }
+}
